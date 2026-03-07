66be590e4a6af8b8f1c53619325450327eec9972
nzhukovsk-bit <nzhukovsk@gmail.com>
2026-03-07 11:46:05 +0000

    Файл hello_world.cpp (плохой стиль кода)

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,15 +1,5 @@
 #include <iostream>
-#include <string>
-
-
+using namespace std;   
 int main()
-{
-    string name;
-    
-    cout << "Enter your name: ";
-    cin >> name;
-    
-    cout << "Hello world from " << name << endl;
-    
-    return 0;
-}
+{    cout << "Hello, World!" << endl;
+    return 0;}
